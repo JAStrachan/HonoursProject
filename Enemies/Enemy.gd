b06dc874a0b9b2c3cf799e8c1655f811567c95c3
JAStrachan <jstrachansss@gmail.com>
2019-02-06 19:36:57 +0000

    Enemies looks at player when in a certain range

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -2,7 +2,10 @@ extends KinematicBody2D
 
 export (int) var health = 100
 export (int) var bulletDamage = 50
+export (int) var vision_distance = 100
 var velocity = Vector2()
+
+onready var player = $"../Player"
 
 signal enemy_death
 
@@ -21,8 +24,16 @@ func bullet_hit():
 # Used for attacking the player through melee damage, currently in development as a figure out best practice	
 func hit_player():
 	pass
-		
+
 func _physics_process(delta):
+	# calculating the direction and distance from the player
+	
+	var direction = player.position - self.position
+	var distance = sqrt(direction.x * direction.x + direction.y * direction.y)
+	if distance < vision_distance:
+		look_at(player.position)
+		
+	rotate(rotation * delta) # rotates the character independant of its movement
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.normal)
