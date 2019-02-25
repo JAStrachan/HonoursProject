df69fbdff185c556f51568117b7ebbeb7dd5b38f
JAStrachan <jstrachansss@gmail.com>
2019-02-25 23:45:08 +0000

    Got a more functional sight system for enemies plus debugging materials

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -2,7 +2,9 @@ extends KinematicBody2D
 
 export (int) var health = 100
 export (int) var bulletDamage = 50
-export (int) var vision_distance = 100
+export (int) var vision_distance = 150
+
+var raycast_hit_pos = [] # the positions the raycasts have hit
 
 var detection_area_colour = Color(.867, .91, .247, 0.1)
 var raycast_debug_colour = Color(1,0,0,1)
@@ -10,11 +12,7 @@ var debug	# if we are in debugging state or not
 
 var velocity = Vector2()
 
-var raycast_hit_pos # the positions the raycasts have hit
-
 var target	 # who we are shooting at
-
-onready var player = $"../Player"
 
 signal enemy_death
 
@@ -52,29 +50,53 @@ func _physics_process(delta):
 			
 			
 # Used for detecting any threats to itself via raycasting
+# Taken from http://kidscancode.org/blog/2018/03/godot3_visibility_raycasts/
 func detect_enemies():
+	raycast_hit_pos = []
 	var space_state = get_world_2d().direct_space_state
-	# sends out raycasts into the world
-	var result = space_state.intersect_ray(position, target.position, [self], collision_mask)
-	if result:
-		raycast_hit_pos = result.position
-		if result.collider.name == 'Player':
-			rotation = (target.position - position).angle()
+	var radius = 25 # TODO set this to 16 when you have resized the sprites
+	var north = target.position + Vector2(0, radius)
+	var south = target.position + Vector2(0, -radius)
+	var west = target.position + Vector2(-radius, 0)
+	var east = target.position + Vector2(radius, 0)
 	
+	for pos in [target.position, north, south, west, east]:
+		# sends out raycasts into the world checking the corners and centre of the threat
+		var result = space_state.intersect_ray(position, pos, [self], collision_mask)
+		if result:
+			raycast_hit_pos.append(result.position)
+			if result.collider.name == 'Player':
+				rotation = (target.position - position).angle()
+				break
+	
+# for squares
+#func get_edges_of_target():
+## The corners of the threat
+#	var target_extents = target.get_node('CollisionShape2D').shape.extents - Vector2(5, 5)
+#	var nw = target.position - target_extents
+#	var se = target.position + target_extents
+#	var ne = target.position + Vector2(target_extents.x, -target_extents.y)
+#	var sw = target.position + Vector2(-target_extents.x, target_extents.y)
+
 func _draw():
     # display the visibility area
 	# this debugging code stuff is from http://kidscancode.org/blog/2018/03/godot3_visibility_raycasts/
 	draw_circle(Vector2(), vision_distance, detection_area_colour)
+	var radius = 25 
+	
 	if target:
-		draw_line(Vector2(), (raycast_hit_pos - position).rotated(-rotation), raycast_debug_colour)
-		draw_circle((raycast_hit_pos - position).rotated(-rotation), 5, raycast_debug_colour)
-
-# Calculating the direction and distance from the player
-func detect_player():
-	var direction = player.position - self.position
-	var distance = sqrt(direction.x * direction.x + direction.y * direction.y)
-	if distance < vision_distance:
-		look_at(player.position)
+		# shows collision points
+		var north = target.position + Vector2(0, radius)
+		var south = target.position + Vector2(0, -radius)
+		var west = target.position + Vector2(-radius, 0)
+		var east = target.position + Vector2(radius, 0)
+		
+		for pos in [ north, south, west, east ]: 
+			draw_circle((pos - position).rotated(-rotation), 5, raycast_debug_colour)
+			
+		for hit in raycast_hit_pos:
+			draw_line(Vector2(), (hit - position).rotated(-rotation), raycast_debug_colour)
+			draw_circle((hit - position).rotated(-rotation), 5, raycast_debug_colour)
 
 # TODO Sort these out with threat models so they can rank the threats
 func _on_AreaDetection_body_entered(body):
@@ -83,7 +105,6 @@ func _on_AreaDetection_body_entered(body):
 		if target:
 			return
 		target = body
-
 
 func _on_AreaDetection_body_exited(body):
 	if body == target:
