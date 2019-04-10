afd4c1c3b71c3b55d9daf30eaa5142e9bbf0930c
JAStrachan <jstrachansss@gmail.com>
2019-04-10 11:47:09 +0100

    finished the making of the map

--- TargetFound.gd
+++ TargetFound.gd
@@ -1,9 +1,9 @@
 extends "res://addons/godot-behavior-tree-plugin/condition.gd"
 
 func tick(tick):
-	if tick.blackboard.get("target"):
+#	if tick.blackboard.get("target"):
+#		return OK
+	if tick.actor.target:
 		return OK
-	#if tick.actor.target:
-	#	return OK
 	else:
 		return FAILED
